46707e48d27d5fda7e0a2f6dd7fe0ee8a3560b6c
zeeshan <zeeshankhanaptech@gmail.com>
2019-11-23 18:03:22 +0500

    calculator final

--- script.js
+++ script.js
@@ -1,21 +1,75 @@
+localStorage.setItem("sound",true)
+
+
+function as(a){
+
+const aa= localStorage.getItem("sound")
+console.log(aa)
+if(aa=="true")
+{
+
+  const mySound = document.getElementById("sound")
+  mySound.src=a  
+    mySound.play()
+  }
+} 
+  function toggle()
+  {
+
+
+    if(document.getElementById("1").value=="OFF"){
+      document.getElementById("1").value="ON";
+      localStorage.setItem("sound",true)
+    
+    }
+   
+     else if(document.getElementById("1").value=="ON"){
+      document.getElementById("1").value="OFF";
+      localStorage.setItem("sound",false)
+    }
+   
+  }
 console.log("locals",localStorage.getItem("age"))
 if(localStorage.getItem("age"))
 {
     let age=localStorage.getItem("age")
     if(age>25)
     {
+     
 
     document.getElementById("display").style.fontSize="48px"
-    document.getElementsByClassName("btnNum")[0].style.fontSize="21px"
-    document.getElementsByClassName("btnNum")[1].style.fontSize="21px"
-    document.getElementsByClassName("btnNum")[2].style.fontSize="21px"
-    document.getElementsByClassName("btnNum")[3].style.fontSize="21px"
-    document.getElementsByClassName("btnNum")[4].style.fontSize="21px"
-    document.getElementsByClassName("btnNum")[5].style.fontSize="21px"
-    document.getElementsByClassName("btnNum")[6].style.fontSize="21px"
-    document.getElementsByClassName("btnNum")[7].style.fontSize="21px"
-    document.getElementsByClassName("btnNum")[8].style.fontSize="21px"
-    document.getElementsByClassName("btnNum")[9].style.fontSize="21px"
+    document.getElementsByClassName("btnMath")[0].style.fontSize="25px"
+    document.getElementsByClassName("btnMath")[1].style.fontSize="25px"
+    document.getElementsByClassName("btnMath")[2].style.fontSize="25px"
+    document.getElementsByClassName("btnMath")[3].style.fontSize="25px"
+    document.getElementsByClassName("btnMath")[4].style.fontSize="25px"
+    document.getElementsByClassName("btnMath")[5].style.fontSize="25px"
+    document.getElementsByClassName("btnMath")[6].style.fontSize="25px"
+    document.getElementsByClassName("btnMath")[7].style.fontSize="25px"
+    document.getElementsByClassName("btnMath")[8].style.fontSize="25px"
+    document.getElementsByClassName("btnMath")[9].style.fontSize="25px"
+
+
+    document.getElementsByClassName("btnNum")[0].style.fontSize="45px"
+    document.getElementsByClassName("btnNum")[1].style.fontSize="45px"
+    document.getElementsByClassName("btnNum")[2].style.fontSize="45px"
+    document.getElementsByClassName("btnNum")[3].style.fontSize="45px"
+    document.getElementsByClassName("btnNum")[4].style.fontSize="45px"
+    document.getElementsByClassName("btnNum")[5].style.fontSize="45px"
+    document.getElementsByClassName("btnNum")[6].style.fontSize="45px"
+    document.getElementsByClassName("btnNum")[7].style.fontSize="45px"
+    document.getElementsByClassName("btnNum")[8].style.fontSize="45px"
+    document.getElementsByClassName("btnNum")[9].style.fontSize="45px"
+
+
+    document.getElementsByClassName("btnTop")[0].style.fontSize="30px"
+    document.getElementsByClassName("btnTop")[1].style.fontSize="30px"
+    document.getElementsByClassName("btnTop")[2].style.fontSize="30px"
+
+
+    document.getElementsByClassName("btnOpps")[0].style.fontSize="30px"
+    document.getElementsByClassName("btnOpps")[1].style.fontSize="30px"
+    document.getElementsByClassName("btnOpps")[2].style.fontSize="30px"
     // document.getElementsByClassName("btnNum")[10].style.fontSize="21px"
     }
 }
@@ -27,49 +81,41 @@ function playAudio(url) {
 
 
 function addChar(input, character) {
-	if(input.value == null || input.value == "0")
-		input.value = character
+  as("c.wav")
+  if(input.value == null || input.value == "0")
+    input.value = character
+    
 	else
 		input.value += character
 }
 
 
-function cos(form) {
-   
-	form.display.value = Math.cos(form.display.value);
-}
 
-function sin(form) {
-	form.display.value = Math.sin(form.display.value);
-}
-
-function tan(form) {
-	form.display.value = Math.tan(form.display.value);
-}
 
 function sqrt(form) {
-	form.display.value = Math.sqrt(form.display.value);
+  form.display.value = Math.sqrt(form.display.value);
+  as("c.wav")
 }
 
 function ln(form) {
-	form.display.value = Math.log(form.display.value);
+  form.display.value = Math.log(form.display.value);
+  as("c.wav")
 }
 
 function exp(form) {
     form.display.value = Math.exp(form.display.value);
-    const mySound = document.getElementById("sound");  
-    mySound.play() 
+    as("c.wav")
 }
 
 function deleteChar(input) {
     input.value = input.value.substring(0, input.value.length - 1)
-    const mySound = document.getElementById("sound");  
-    mySound.play() 
+    as("b.wav")
 }
 var val = 0.0;
 function percent(input) {
   val = input.value;
   input.value = input.value + "%";
+  as("c.wav")
 }
 
 function changeSign(input) {
@@ -87,18 +133,21 @@ function compute(form) {
     //val = 0.0;
  // } else 
     form.display.value = eval(form.display.value);
-    const mySound = document.getElementById("sound");  
-    mySound.play() 
+    as("b.wav")
   }
 
 
 function square(form) {
-	form.display.value = eval(form.display.value) * eval(form.display.value)
-    const mySound = document.getElementById("sound");  
-    mySound.play() 
+	// form.display.value = eval(form.display.value) * eval(form.display.value)
+  //   const mySound = document.getElementById("sound");  
+  //   mySound.play() 
+  as("c.wav")
 }
 
+
 function checkNum(str) {
+  
+   
 	for (var i = 0; i < str.length; i++) {
 		var ch = str.charAt(i);
 		if (ch < "0" || ch > "9") {
